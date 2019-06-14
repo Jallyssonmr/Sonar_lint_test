126b85f077c50c28a199e57e7d3df82e8ab4beb8
Jallysson <jallysson_miranda@outlook.com>
2019-06-14 15:16:12 -0300

    Exemplo de um erro de variável não usada

---
+++ main.py
@@ -0,0 +1,10 @@
+def main(object):
+    def __init__(self):
+        variavel_nao_usada = "e agora jose"
+        string = "jallysson, 26"
+    
+        self.get_name_test(string)
+    
+    def get_name_test(self, string):
+        name = string.split(",")[0]
+        return name
